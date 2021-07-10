8a68a9ec632566c43c7a1f4bb8826d97ac87ddc8
Tikhon Petrishchev <ticha56@mail.ru>
2021-07-10 21:04:52 +0300

    fixed development sockets

--- heytelepat/heytelepat/asgi.py
+++ heytelepat/heytelepat/asgi.py
@@ -7,19 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import os
 import django
-from django.core.asgi import get_asgi_application
+django.setup()
 
+from django.core.asgi import get_asgi_application
+from speakerapi.routing import ws_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from speakerapi.routing import ws_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heytelepat.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
 })
+
