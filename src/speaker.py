02ead9576ee31bc416fbbd2ad22e58069d052a02
Tikhon <tikhon.petrishchev@gmail.com>
2021-08-27 14:04:46 +0300

    v0.3.0 - Added new authentication system

--- src/speaker.py
+++ src/speaker.py
@@ -6,7 +6,7 @@ Input point for Telepat Speaker
 OOO Telepat, All Rights Reserved
 """
 
-__version__ = '0.2.12'
+__version__ = '0.3.0'
 __author__ = 'Tikhon Petrishchev'
 __credits__ = 'TelePat LLC'
 
