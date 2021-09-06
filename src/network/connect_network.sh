156dab144d29aea9fa0a000a15a8ebc858346584
Tikhon <tikhon.petrishchev@gmail.com>
2021-09-06 17:47:32 +0300

    Fixed network sudo

--- src/network/connect_network.sh
+++ src/network/connect_network.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo wpa_cli -i wlan0 reconfigure || (
-  systemctl daemon-reload
+  sudo systemctl daemon-reload
   sudo sudo systemctl restart dhcpcd
   sudo wpa_cli -i wlan0 reconfigure
 )
